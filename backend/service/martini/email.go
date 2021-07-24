d659f29de6f9efb5b66f3d46fe5d0edeb2950e2a
Denis Issoupov   <denis@ekspand.com>
2021-07-24 15:05:57 -0700

    Org validation EMAILs

--- backend/service/martini/email.go
+++ backend/service/martini/email.go
@@ -5,6 +5,7 @@ import (
 	"text/template"
 
 	"github.com/ekspand/trusty/pkg/email"
+	"github.com/go-phorce/dolly/xlog"
 	"github.com/juju/errors"
 )
 
@@ -16,25 +17,27 @@ func renderEmailTemplate(html string, data interface{}) (string, error) {
 	}
 
 	var buf bytes.Buffer
-
 	if err := tmpl.Execute(&buf, data); err != nil {
 		return "", errors.Annotatef(err, "failed to render email template")
-
 	}
+
 	return buf.String(), nil
 }
 
 // sendEmail used to send email with Mailgun provider
-func (s *Service) sendEmail(toEmail string, subject, string, htmlTemplate string, data interface{}) error {
+func (s *Service) sendEmail(toEmail, subject, htmlTemplate string, data interface{}) error {
 	body, err := renderEmailTemplate(htmlTemplate, data)
-
 	if err != nil {
 		return errors.Annotatef(err, "failed to render email template")
 	}
 
-	err = s.emailProv.GetProvider(email.MailgunProviderName).Send(toEmail, subject, body)
-	if err != nil {
-		return errors.Annotatef(err, "failed to send email to %q", toEmail)
+	if !s.disableEmail {
+		logger.KV(xlog.NOTICE, "email", toEmail, subject, subject)
+
+		err = s.emailProv.GetProvider(email.MailgunProviderName).Send(toEmail, subject, body)
+		if err != nil {
+			return errors.Annotatef(err, "failed to send email to %q", toEmail)
+		}
 	}
 	return nil
 }
