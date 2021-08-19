6c00d8ce6c7dbfad3ad8201c0343aa5b5ec90bda
Denis Issoupov   <denis@ekspand.com>
2021-08-19 11:35:20 -0700

    Use global IDGenerator, added perf test

--- internal/appcontainer/appcontainer_test.go
+++ internal/appcontainer/appcontainer_test.go
@@ -3,12 +3,14 @@ package appcontainer
 import (
 	"os"
 	"path"
+	"sync"
 	"testing"
 
 	"github.com/ekspand/trusty/internal/config"
 	"github.com/go-phorce/dolly/algorithms/guid"
 	"github.com/go-phorce/dolly/audit"
 	"github.com/go-phorce/dolly/tasks"
+	"github.com/juju/errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -68,4 +70,43 @@ func TestNewContainerFactory(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestIdGenerator(t *testing.T) {
+	used := map[uint64]bool{}
+	var lock sync.RWMutex
+
+	useCode := func(code uint64) error {
+		lock.Lock()
+		defer lock.Unlock()
+
+		if used[code] {
+			return errors.Errorf("duplicate: %d", code)
+		}
+		used[code] = true
+		return nil
+	}
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < 50; i++ {
+		go func() {
+			defer wg.Done()
+			wg.Add(1)
+
+			for c := 0; c < 2000; c++ {
+				id, err := IDGenerator.NextID()
+				assert.NoError(t, err)
+				if err != nil {
+					return
+				}
+				err = useCode(id)
+				assert.NoError(t, err)
+				if err != nil {
+					return
+				}
+			}
+		}()
+	}
+	wg.Wait()
 }
--- tests/testutils/idgen.go
+++ tests/testutils/idgen.go
@@ -1,21 +1,11 @@
 package testutils
 
 import (
-	"os"
-	"time"
-
+	"github.com/ekspand/trusty/internal/appcontainer"
 	"github.com/ekspand/trusty/internal/db"
-	"github.com/sony/sonyflake"
 )
 
-var idGenerator = sonyflake.NewSonyflake(sonyflake.Settings{
-	StartTime: time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
-	MachineID: func() (uint16, error) {
-		return uint16(os.Getpid()), nil
-	},
-})
-
-// IDGenerator returns static ID generator for the process
+// IDGenerator returns static ID generator for the app
 func IDGenerator() db.IDGenerator {
-	return idGenerator
+	return appcontainer.IDGenerator
 }
