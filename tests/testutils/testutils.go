2895cd5eb86d440bc80659e568a85e33208c7824
Denis Issoupov <denis@ekspand.com>
2022-05-19 15:00:46 +0100

    test: CreateURL

--- tests/testutils/testutils.go
+++ tests/testutils/testutils.go
@@ -1,23 +1,16 @@
 package testutils
 
 import (
-	"fmt"
-	"math/rand"
 	"os"
 	"path/filepath"
-	"sync/atomic"
 
+	"github.com/effective-security/porto/tests/testutils"
 	"github.com/martinisecurity/trusty/backend/config"
-)
-
-var (
-	nextPort = int32(os.Getpid()%10000) + int32(17891) + rand.Int31n(1000)
 )
 
 // CreateURLs returns URL with a random port
 func CreateURLs(scheme, host string) string {
-	next := atomic.AddInt32(&nextPort, 1)
-	return fmt.Sprintf("%s://%s:%d", scheme, host, next)
+	return testutils.CreateURL(scheme, host)
 }
 
 // LoadConfig returns Configuration
