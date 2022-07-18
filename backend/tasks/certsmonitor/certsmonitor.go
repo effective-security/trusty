af1c454081b0d39e09a1cee6925547e7b29c945e
Denis Issoupov <denis@ekspand.com>
2022-07-18 14:08:19 -0700

    certmonitor: removed config

--- backend/tasks/certsmonitor/certsmonitor.go
+++ backend/tasks/certsmonitor/certsmonitor.go
@@ -5,8 +5,6 @@ import (
 	"time"
 
 	"github.com/effective-security/porto/pkg/tasks"
-	"github.com/effective-security/porto/x/fileutil"
-	"github.com/effective-security/trusty/backend/config"
 	"github.com/effective-security/xlog"
 	"github.com/effective-security/xpki/certutil"
 	"github.com/pkg/errors"
@@ -26,7 +24,6 @@ const (
 
 // Task defines the healthcheck task
 type Task struct {
-	conf     *config.Configuration
 	name     string
 	schedule string
 	certsMap map[string]string // map => location:type
@@ -72,33 +69,17 @@ func certsMapFromLocations(locations []string) map[string]string {
 
 func create(
 	name string,
-	conf *config.Configuration,
 	schedule string,
 	args ...string,
 ) (*Task, error) {
 	task := &Task{
-		conf:     conf,
 		name:     name,
 		schedule: schedule,
 	}
 
-	if conf != nil {
-		task.certsMap = certsMapFromLocations(args)
-		if !conf.TrustyClient.ClientTLS.Empty() &&
-			fileutil.FileExists(conf.TrustyClient.ClientTLS.CertFile) == nil {
-			task.certsMap[conf.TrustyClient.ClientTLS.CertFile] = typClient
-		}
-
-		for _, srv := range conf.HTTPServers {
-			if !srv.Disabled && !srv.ServerTLS.Empty() &&
-				fileutil.FileExists(srv.ServerTLS.CertFile) == nil {
-				task.certsMap[srv.ServerTLS.CertFile] = typServer
-			}
-		}
-
-		for location, typ := range task.certsMap {
-			logger.Infof("type=%q, location=%q,", typ, location)
-		}
+	task.certsMap = certsMapFromLocations(args)
+	for location, typ := range task.certsMap {
+		logger.Infof("type=%q, location=%q,", typ, location)
 	}
 	return task, nil
 }
@@ -110,9 +91,8 @@ func Factory(
 	schedule string,
 	args ...string,
 ) interface{} {
-	return func(cfg *config.Configuration) error {
-
-		task, err := create(name, cfg, schedule, args...)
+	return func() error {
+		task, err := create(name, schedule, args...)
 		if err != nil {
 			return errors.WithStack(err)
 		}
